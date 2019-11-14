991d272f620d24da481b115efd728d0963dbf95f
teslaenergy@gmail.com <teslaenergy@gmail.com>
2019-11-13 17:33:17 -0700

    qtdemo

--- FeatureDemos/emscripten/QTDemo/build.sh
+++ FeatureDemos/emscripten/QTDemo/build.sh
@@ -2,5 +2,7 @@
 
 #../../qtbase/bin/qmake
 
+#get qt 5.13 ./configure -opensource -confirm-license -xplatform wasm-emscripten -release -static -no-feature-thread -nomake examples -no-dbus -no-ssl
+
 /run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/Dev/libs/GUI/qtbase/bin/qmake
 make
