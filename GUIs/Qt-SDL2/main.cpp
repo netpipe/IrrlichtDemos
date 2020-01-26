8bec804910c61170f9b066d7004f5f7a81325122
netpipe <teslaenergy@gmail.com>
2020-01-25 21:45:23 -0700

    qt-sdl

---
+++ GUIs/Qt-SDL2/main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindowFrom((void*)w.ptr_gfx->winId());
+    SDL_Renderer* render = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+    SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
+    SDL_RenderFillRect(render, NULL);
+    SDL_RenderPresent(render);
+
+    return a.exec();
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(render);
+    SDL_Quit();
+}
